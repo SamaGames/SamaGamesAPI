ef6308dda8a8d1e9e58e78c83344bfb0f3c1611f
BlueSlime <jeremylevilain78@orange.fr>
2015-04-12 23:13:34 +0200

    Moar util

--- src/main/java/net/samagames/tools/PlayerUtils.java
+++ src/main/java/net/samagames/tools/PlayerUtils.java
@@ -1,18 +1,31 @@
 package net.samagames.tools;
 
+import net.samagames.api.SamaGamesAPI;
 import net.samagames.permissionsbukkit.PermissionsBukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public class PlayerUtils
 {
+    public static String getFullyFormattedPlayerName(UUID uuid)
+    {
+        return PermissionsBukkit.getPrefix(PermissionsBukkit.getApi().getUser(uuid)) + PermissionsBukkit.getDisplay(PermissionsBukkit.getApi().getUser(uuid)) + SamaGamesAPI.get().getUUIDTranslator().getName(uuid) + ChatColor.RESET;
+    }
+
     public static String getFullyFormattedPlayerName(Player player)
     {
-        return PermissionsBukkit.getPrefix(PermissionsBukkit.getApi().getUser(player.getUniqueId())) + PermissionsBukkit.getDisplay(PermissionsBukkit.getApi().getUser(player.getUniqueId())) + player.getName() + ChatColor.RESET;
+        return getFullyFormattedPlayerName(player.getUniqueId());
     }
     
+    public static String getColoredFormattedPlayerName(UUID uuid)
+    {
+        return PermissionsBukkit.getPrefix(PermissionsBukkit.getApi().getUser(uuid)) + SamaGamesAPI.get().getUUIDTranslator().getName(uuid) + ChatColor.RESET;
+    }
+
     public static String getColoredFormattedPlayerName(Player player)
     {
-        return PermissionsBukkit.getPrefix(PermissionsBukkit.getApi().getUser(player.getUniqueId())) + player.getName() + ChatColor.RESET;
+        return getColoredFormattedPlayerName(player.getUniqueId());
     }
 }
