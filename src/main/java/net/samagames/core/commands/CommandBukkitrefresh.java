2ad519db90a0df6decc9c1a7f2054b24f5953b8b
BlueSlime <jeremylevilain78@orange.fr>
2015-06-27 16:09:42 +0000

    CommandBukkitrefresh.java édité en ligne via Bitbucket

--- src/main/java/net/samagames/core/commands/CommandBukkitrefresh.java
+++ src/main/java/net/samagames/core/commands/CommandBukkitrefresh.java
@@ -4,23 +4,23 @@ import net.samagames.core.APIPlugin;
 import net.samagames.permissionsapi.PermissionsAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 /**
  * Created by zyuiop on 27/08/14.
  */
-public class CommandBukkitrefresh implements CommandExecutor {
+public class CommandBukkitrefresh extends AbstractCommand {
 
     protected PermissionsAPI api;
 
-    public CommandBukkitrefresh(PermissionsAPI api) {
-        this.api = api;
+    public CommandBukkitrefresh(APIPlugin plugin) {
+        super(plugin);
+        
+        this.api = SamaGamesAPI.get().getPermissionsManager().getApi();
     }
 
     @Override
-    public boolean onCommand(final CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(final CommandSender commandSender, String label, String[] strings) {
         Bukkit.getScheduler().runTaskAsynchronously(APIPlugin.getInstance(), new Runnable() {
             @Override
             public void run() {
