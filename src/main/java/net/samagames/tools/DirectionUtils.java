c4d769f5951e13908a23e0505e0869f473dd2f84
BlueSlime <jeremylevilain78@orange.fr>
2015-07-17 15:04:46 +0200

    DirectionUtils

---
+++ src/main/java/net/samagames/tools/DirectionUtils.java
@@ -0,0 +1,29 @@
+package net.samagames.tools;
+
+import org.bukkit.entity.Player;
+
+public class DirectionUtils
+{
+    public enum Directions { NORTH, SOUTH, EAST, WEST };
+
+    public static Directions getPlayerDirection(Player player)
+    {
+        double rotation = (player.getLocation().getYaw() - 90) % 360;
+
+        if (rotation < 0)
+            rotation += 360.0;
+
+        if (0 <= rotation && rotation < 22.5)
+            return Directions.NORTH;
+        else if (67.5 <= rotation && rotation < 112.5)
+            return Directions.EAST;
+        else if (157.5 <= rotation && rotation < 202.5)
+            return Directions.SOUTH;
+        else if (247.5 <= rotation && rotation < 292.5)
+            return Directions.WEST;
+        else if (337.5 <= rotation && rotation < 360.0)
+            return Directions.NORTH;
+        else
+            return null;
+    }
+}
