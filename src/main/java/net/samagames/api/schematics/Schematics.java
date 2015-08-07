a8c97c656d563684122ed9929ca18d81804b3f99
Thog <thog92@hotmail.fr>
2015-08-07 14:49:12 +0200

    Schematics API: Now load chunk before setBlock during paste

--- src/main/java/net/samagames/api/schematics/Schematics.java
+++ src/main/java/net/samagames/api/schematics/Schematics.java
@@ -21,67 +21,66 @@ import java.io.IOException;
 public class Schematics
 {
 
-	/**
-	 * Permite to load a schematic
-	 *
-	 * @param file The schematic
-	 * @return a ISchematic object
-	 * @throws IOException when the file doesn't exist
-	 */
-	public static ISchematic load(File file) throws IOException
-	{
-		if (!file.exists())
-			throw new IOException(file.getAbsolutePath() + ": File not found");
+    /**
+     * Permite to load a schematic
+     *
+     * @param file The schematic
+     * @return a ISchematic object
+     * @throws IOException when the file doesn't exist
+     */
+    public static ISchematic load(File file) throws IOException
+    {
+        if (!file.exists())
+            throw new IOException(file.getAbsolutePath() + ": File not found");
 
-		FileInputStream in = new FileInputStream(file);
-		NBTTagCompound tagCompound = NBTCompressedStreamTools.a(in);
+        FileInputStream in = new FileInputStream(file);
+        NBTTagCompound tagCompound = NBTCompressedStreamTools.a(in);
 
-		// Read schematic size
-		short width = tagCompound.getShort("Width");
-		short length = tagCompound.getShort("Length");
-		short height = tagCompound.getShort("Height");
-		SchematicImpl schematic = new SchematicImpl(width, height, length);
-		byte[] blocks = tagCompound.getByteArray("Blocks");
-		byte[] metadata = tagCompound.getByteArray("Data");
+        // Read schematic size
+        short width = tagCompound.getShort("Width");
+        short length = tagCompound.getShort("Length");
+        short height = tagCompound.getShort("Height");
+        SchematicImpl schematic = new SchematicImpl(width, height, length);
+        byte[] blocks = tagCompound.getByteArray("Blocks");
+        byte[] metadata = tagCompound.getByteArray("Data");
 
-		short originX = tagCompound.getShort("WEOriginX");
-		short originY = tagCompound.getShort("WEOriginY");
-		short originZ = tagCompound.getShort("WEOriginZ");
-		schematic.setOrigin(originX, originY, originZ);
+        short originX = tagCompound.getShort("WEOriginX");
+        short originY = tagCompound.getShort("WEOriginY");
+        short originZ = tagCompound.getShort("WEOriginZ");
+        schematic.setOrigin(originX, originY, originZ);
 
-		short offsetX = tagCompound.getShort("WEOffsetX");
-		short offsetY = tagCompound.getShort("WEOffsetY");
-		short offsetZ = tagCompound.getShort("WEOffsetZ");
-		schematic.setOffset(offsetX, offsetY, offsetZ);
+        short offsetX = tagCompound.getShort("WEOffsetX");
+        short offsetY = tagCompound.getShort("WEOffsetY");
+        short offsetZ = tagCompound.getShort("WEOffsetZ");
+        schematic.setOffset(offsetX, offsetY, offsetZ);
 
-		// Detect Blocks and metadata
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				for (int z = 0; z < length; z++)
-				{
-					int index = x + (y * length + z) * width;
-					Material material = Material.getMaterial(blocks[index] & 0xFF);
-					byte meta = (byte) (metadata[index] & 0xFF);
-					schematic.setMaterial(x, y, z, material, meta);
-				}
-			}
-		}
+        // Detect Blocks and metadata
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < length; z++)
+                {
+                    int index = x + (y * length + z) * width;
+                    Material material = Material.getMaterial(blocks[index] & 0xFF);
+                    byte meta = (byte) (metadata[index] & 0xFF);
+                    schematic.setMaterial(x, y, z, material, meta);
+                }
+            }
+        }
 
-		NBTTagList tileEntities = tagCompound.getList("TileEntities", 10);
+        NBTTagList tileEntities = tagCompound.getList("TileEntities", 10);
 
-		for (int i = 0; i < tileEntities.size(); i++)
-		{
-			TileEntity tileEntity = TileEntity.c(tileEntities.get(i));
-			if (tileEntity != null)
-			{
-				schematic.setTileEntity(tileEntity.getPosition(), tileEntity);
-			}
-		}
+        for (int i = 0; i < tileEntities.size(); i++)
+        {
+            TileEntity tileEntity = TileEntity.c(tileEntities.get(i));
+            if (tileEntity != null)
+            {
+                schematic.setTileEntity(tileEntity.getPosition(), tileEntity);
+            }
+        }
 
-
-		in.close();
-		return schematic;
-	}
+        in.close();
+        return schematic;
+    }
 }
--- src/main/java/net/samagames/api/schematics/internal/SchematicImpl.java
+++ src/main/java/net/samagames/api/schematics/internal/SchematicImpl.java
@@ -20,143 +20,145 @@ import java.util.Map;
 public final class SchematicImpl implements ISchematic
 {
 
-	private final int width, length, height;
-	private final int[][][] blocks;
-	private final byte[][][] metadata;
-	private final Map<BlockPosition, TileEntity> tileEntities;
-	private BlockPosition origin, offset;
+    private final int width, length, height;
+    private final int[][][] blocks;
+    private final byte[][][] metadata;
+    private final Map<BlockPosition, TileEntity> tileEntities;
+    private BlockPosition origin, offset;
 
-	// Internal Class: DO NOT INSTANCE IT
-	public SchematicImpl(final int width, final int height, final int length)
-	{
-		this.width = width;
-		this.length = length;
-		this.height = height;
+    // Internal Class: DO NOT INSTANCE IT
+    public SchematicImpl(final int width, final int height, final int length)
+    {
+        this.width = width;
+        this.length = length;
+        this.height = height;
 
-		this.blocks = new int[width][height][length];
-		this.metadata = new byte[width][height][length];
-		this.tileEntities = new HashMap<>();
-		this.origin = BlockPosition.ZERO;
-		this.offset = BlockPosition.ZERO;
-	}
+        this.blocks = new int[width][height][length];
+        this.metadata = new byte[width][height][length];
+        this.tileEntities = new HashMap<>();
+        this.origin = BlockPosition.ZERO;
+        this.offset = BlockPosition.ZERO;
+    }
 
-	@Override
-	public Material getMaterial(int x, int y, int z)
-	{
-		return isValid(x, y, z) ? Material.getMaterial(this.blocks[x][y][z]) : Material.AIR;
-	}
+    @Override
+    public Material getMaterial(int x, int y, int z)
+    {
+        return isValid(x, y, z) ? Material.getMaterial(this.blocks[x][y][z]) : Material.AIR;
+    }
 
-	@Override
-	public void setMaterial(int x, int y, int z, Material material, byte metadata)
-	{
-		if (isValid(x, y, z))
-		{
-			this.blocks[x][y][z] = material.getId();
-			this.metadata[x][y][z] = metadata;
-		}
-	}
+    @Override
+    public void setMaterial(int x, int y, int z, Material material, byte metadata)
+    {
+        if (isValid(x, y, z))
+        {
+            this.blocks[x][y][z] = material.getId();
+            this.metadata[x][y][z] = metadata;
+        }
+    }
 
-	@Override
-	public int getWidth()
-	{
-		return width;
-	}
+    @Override
+    public int getWidth()
+    {
+        return width;
+    }
 
-	@Override
-	public int getLength()
-	{
-		return length;
-	}
+    @Override
+    public int getLength()
+    {
+        return length;
+    }
 
-	@Override
-	public int getHeight()
-	{
-		return height;
-	}
+    @Override
+    public int getHeight()
+    {
+        return height;
+    }
 
-	@Override
-	public List<Material> getBlocks()
-	{
-		return null;
-	}
+    @Override
+    public List<Material> getBlocks()
+    {
+        return null;
+    }
 
-	@Override
-	public void paste(Location location)
-	{
-		this.fill(location);
-		net.minecraft.server.v1_8_R3.World world = ((CraftWorld) location.getWorld()).getHandle();
+    @Override
+    public void paste(Location location)
+    {
+        this.fill(location);
+        net.minecraft.server.v1_8_R3.World world = ((CraftWorld) location.getWorld()).getHandle();
 
-		int worldX = location.getBlockX();
-		int worldY = location.getBlockY();
-		int worldZ = location.getBlockZ();
+        int worldX = location.getBlockX();
+        int worldY = location.getBlockY();
+        int worldZ = location.getBlockZ();
 
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				for (int z = 0; z < length; z++)
-				{
-					Block block = (isValid(x, y, z) ? Block.getById(this.blocks[x][y][z]) : Blocks.AIR);
-					BlockPosition worldPosition = new BlockPosition(worldX + x, worldY + y, worldZ + z).a(offset);
-					BlockPosition internalPosition = new BlockPosition(x, y, z);
-					world.setTypeAndData(worldPosition, block.fromLegacyData(this.metadata[x][y][z]), 3);
-					if (this.tileEntities.containsKey(internalPosition))
-						world.setTileEntity(worldPosition, this.tileEntities.get(internalPosition));
-				}
-			}
-		}
-	}
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < length; z++)
+                {
+                    Block block = (isValid(x, y, z) ? Block.getById(this.blocks[x][y][z]) : Blocks.AIR);
+                    BlockPosition worldPosition = new BlockPosition(worldX + x, worldY + y, worldZ + z).a(offset);
+                    if (!world.isLoaded(worldPosition))
+                        world.N().getOrCreateChunk(worldPosition.getX() / 16, worldPosition.getY() / 16);
+                    BlockPosition internalPosition = new BlockPosition(x, y, z);
+                    world.setTypeAndData(worldPosition, block.fromLegacyData(this.metadata[x][y][z]), 3);
+                    if (this.tileEntities.containsKey(internalPosition))
+                        world.setTileEntity(worldPosition, this.tileEntities.get(internalPosition));
+                }
+            }
+        }
+    }
 
-	@Override
-	public void fill(Location location)
-	{
-		net.minecraft.server.v1_8_R3.World world = ((CraftWorld) location.getWorld()).getHandle();
+    @Override
+    public void fill(Location location)
+    {
+        net.minecraft.server.v1_8_R3.World world = ((CraftWorld) location.getWorld()).getHandle();
 
-		int worldX = location.getBlockX();
-		int worldY = location.getBlockY();
-		int worldZ = location.getBlockZ();
-		IBlockData air = Blocks.AIR.getBlockData();
-		for (int x = width; x > 0; x--)
-		{
-			for (int y = height; y > 0; y--)
-			{
-				for (int z = length; z > 0; z--)
-				{
-					BlockPosition worldPosition = new BlockPosition(worldX + x, worldY + y, worldZ + z).a(offset);
-					world.setTypeAndData(worldPosition, air, 3);
-				}
-			}
-		}
-	}
+        int worldX = location.getBlockX();
+        int worldY = location.getBlockY();
+        int worldZ = location.getBlockZ();
+        IBlockData air = Blocks.AIR.getBlockData();
+        for (int x = width; x > 0; x--)
+        {
+            for (int y = height; y > 0; y--)
+            {
+                for (int z = length; z > 0; z--)
+                {
+                    BlockPosition worldPosition = new BlockPosition(worldX + x, worldY + y, worldZ + z).a(offset);
+                    world.setTypeAndData(worldPosition, air, 3);
+                }
+            }
+        }
+    }
 
-	private boolean isValid(final int x, final int y, final int z)
-	{
-		return !(x < 0 || y < 0 || z < 0 || x >= this.width || y >= this.height || z >= this.length);
-	}
+    private boolean isValid(final int x, final int y, final int z)
+    {
+        return !(x < 0 || y < 0 || z < 0 || x >= this.width || y >= this.height || z >= this.length);
+    }
 
-	public void setTileEntity(BlockPosition position, TileEntity tileEntity)
-	{
-		if (!tileEntities.containsKey(position))
-		{
-			tileEntities.put(position, tileEntity);
-		}
-	}
-
-
-	public void setOffset(int x, int y, int z)
-	{
-		this.offset = new BlockPosition(x, y, z);
-	}
-
-	public void setOrigin(int x, int y, int z)
-	{
-		this.origin = new BlockPosition(x, y, z);
-	}
+    public void setTileEntity(BlockPosition position, TileEntity tileEntity)
+    {
+        if (!tileEntities.containsKey(position))
+        {
+            tileEntities.put(position, tileEntity);
+        }
+    }
 
 
-	@Override
-	public String toString()
-	{
-		return String.format("SchematicImpl[width: %d, height: %d, length: %d]", width, height, length);
-	}
+    public void setOffset(int x, int y, int z)
+    {
+        this.offset = new BlockPosition(x, y, z);
+    }
+
+    public void setOrigin(int x, int y, int z)
+    {
+        this.origin = new BlockPosition(x, y, z);
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return String.format("SchematicImpl[width: %d, height: %d, length: %d]", width, height, length);
+    }
 }
