6b80fedf61be8c6e05737d40d375446b83e2e188
Amaury Carrade <amaury@carrade.eu>
2015-09-06 14:34:47 +0200

    Rewrote the ActionBar API to a new, more optimized and with more tools one.
    * NEW: the ActionBar API's classes are only loaded once.
    * NEW: added utility methods to send permanent action bar messages. An underlying task is sending again and over again the messages to the players, every 30 ticks, to mimic a constant action bar message. The sender task only runs if needed.

--- src/main/java/net/samagames/tools/chat/ActionBarAPI.java
+++ src/main/java/net/samagames/tools/chat/ActionBarAPI.java
@@ -1,56 +1,252 @@
 package net.samagames.tools.chat;
 
+import net.samagames.api.SamaGamesAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class ActionBarAPI
+
+/**
+ * An utility class to send action bars to the players.
+ */
+public final class ActionBarAPI
 {
-    private static String nmsver;
+	private static boolean enabled = true;
 
-    static {
-        nmsver = Bukkit.getServer().getClass().getPackage().getName();
-        nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
-    }
+	private static String nmsVersion;
 
-    public static void sendActionBar(Player player, String message)
-    {
-        try
-        {
-            Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
-            Object p = c1.cast(player);
-            Object ppoc = null;
-            Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
-            Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
+	private static Class<?> craftPlayerClass;
+	private static Class<?> packetPlayOutChatClass;
+	private static Class<?> packetClass;
+	private static Class<?> chatSerializerClass;
+	private static Class<?> iChatBaseComponentClass;
+	private static Class<?> chatComponentTextClass;
 
-            if (nmsver.equalsIgnoreCase("v1_8_R1") || !nmsver.startsWith("v1_8_"))
-            {
-                Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
-                Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                Method m3 = c2.getDeclaredMethod("a", new Class<?>[] {String.class});
-                Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
-                ppoc = c4.getConstructor(new Class<?>[] {c3, byte.class}).newInstance(new Object[] {cbc, (byte) 2});
-            }
-            else
-            {
-                Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
-                Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                Object o = c2.getConstructor(new Class<?>[] {String.class}).newInstance(new Object[] {message});
-                ppoc = c4.getConstructor(new Class<?>[] {c3, byte.class}).newInstance(new Object[] {o, (byte) 2});
-            }
+	private static Map<UUID, String> actionMessages = new ConcurrentHashMap<>();
 
-            Method m1 = c1.getDeclaredMethod("getHandle", new Class<?>[] {});
-            Object h = m1.invoke(p);
-            Field f1 = h.getClass().getDeclaredField("playerConnection");
-            Object pc = f1.get(h);
-            Method m5 = pc.getClass().getDeclaredMethod("sendPacket",new Class<?>[] {c5});
-            m5.invoke(pc, ppoc);
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-    }
+	private static Runnable actionMessagesUpdater = null;
+	private static boolean actionMessagesUpdaterRunning = false;
+	private static BukkitTask actionMessagesUpdaterTask = null;
+
+	static
+	{
+		nmsVersion = Bukkit.getServer().getClass().getPackage().getName();
+		nmsVersion = nmsVersion.substring(nmsVersion.lastIndexOf(".") + 1);
+
+		try
+		{
+			iChatBaseComponentClass = Class.forName("net.minecraft.server." + nmsVersion + ".IChatBaseComponent");
+			packetPlayOutChatClass = Class.forName("net.minecraft.server." + nmsVersion + ".PacketPlayOutChat");
+			craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsVersion + ".entity.CraftPlayer");
+			packetClass = Class.forName("net.minecraft.server." + nmsVersion + ".Packet");
+
+			if (nmsVersion.equalsIgnoreCase("v1_8_R1") || !nmsVersion.startsWith("v1_8_"))
+			{
+				chatSerializerClass = Class.forName("net.minecraft.server." + nmsVersion + ".ChatSerializer");
+			}
+			else
+			{
+				chatComponentTextClass = Class.forName("net.minecraft.server." + nmsVersion + ".ChatComponentText");
+			}
+		}
+		catch (Exception e)
+		{
+			enabled = false;
+		}
+
+		initActionMessageUpdater();
+	}
+
+	private ActionBarAPI() {}
+
+	/**
+	 * Sends a constant message to the given player.
+	 * <p/>
+	 * This message will remain on the screen until the {@link #removeMessage} method is called,
+	 * or the server is stopped.
+	 *
+	 * @param player  The player.
+	 * @param message The message to display.
+	 */
+	public static void sendPermanentMessage(Player player, String message)
+	{
+		actionMessages.put(player.getUniqueId(), message);
+		sendMessage(player, message);
+
+		checkActionMessageUpdaterRunningState();
+	}
+
+	/**
+	 * Sends a constant message to the given player.
+	 * <p/>
+	 * This message will remain on the screen until the {@link #removeMessage} method is called,
+	 * or the server is stopped.
+	 *
+	 * @param playerUUID The player's UUID.
+	 * @param message    The message to display.
+	 */
+	public static void sendPermanentMessage(UUID playerUUID, String message)
+	{
+		actionMessages.put(playerUUID, message);
+		sendMessage(playerUUID, message);
+
+		checkActionMessageUpdaterRunningState();
+	}
+
+	/**
+	 * Sends an action-bar message to the given player.
+	 * <p/>
+	 * This message will remain approximately three seconds.
+	 *
+	 * @param playerUUID The player's UUID.
+	 * @param message    The message.
+	 */
+	public static void sendMessage(UUID playerUUID, String message)
+	{
+		sendMessage(Bukkit.getPlayer(playerUUID), message);
+	}
+
+	/**
+	 * Sends an action-bar message to the given player.
+	 * <p/>
+	 * This message will remain approximately three seconds.
+	 *
+	 * @param player  The player.
+	 * @param message The message.
+	 *
+	 * @author ConnorLinfoot (https://github.com/ConnorLinfoot/ActionBarAPI/).
+	 */
+	public static void sendMessage(Player player, String message)
+	{
+		if (!enabled || player == null || message == null) return;
+
+		try
+		{
+			Object craftPlayer = craftPlayerClass.cast(player);
+			Object chatPacket;
+
+			if (nmsVersion.equalsIgnoreCase("v1_8_R1") || !nmsVersion.startsWith("v1_8_"))
+			{
+				Method m3 = chatSerializerClass.getDeclaredMethod("a", String.class);
+				Object cbc = iChatBaseComponentClass.cast(m3.invoke(chatSerializerClass, "{\"text\": \"" + message + "\"}"));
+				chatPacket = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(cbc, (byte) 2);
+			}
+			else
+			{
+				Object o = chatComponentTextClass.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+				chatPacket = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(o, (byte) 2);
+			}
+
+			Object handle = craftPlayerClass.getDeclaredMethod("getHandle").invoke(craftPlayer);
+			Object playerConnection = handle.getClass().getDeclaredField("playerConnection").get(handle);
+
+			playerConnection.getClass().getDeclaredMethod("sendPacket", packetClass).invoke(playerConnection, chatPacket);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Removes the action bar message displayed to the given player.
+	 *
+	 * @param player  The player.
+	 * @param instant If {@code true}, the message will be removed instantly. Else, it will dismiss progressively.
+	 *                Please note that in that case, the message may be displayed a few more seconds.
+	 */
+	public static void removeMessage(Player player, boolean instant)
+	{
+		actionMessages.remove(player.getUniqueId());
+
+		if (instant)
+		{
+			sendMessage(player, "");
+		}
+
+		checkActionMessageUpdaterRunningState();
+	}
+
+	/**
+	 * Removes the action bar message displayed to the given player.
+	 *
+	 * @param playerUUID The UUID of the player.
+	 * @param instant    If {@code true}, the message will be removed instantly. Else, it will dismiss progressively.
+	 *                   Please note that in that case, the message may be displayed a few more seconds.
+	 */
+	public static void removeMessage(UUID playerUUID, boolean instant)
+	{
+		actionMessages.remove(playerUUID);
+
+		if (instant)
+		{
+			sendMessage(playerUUID, "");
+		}
+
+		checkActionMessageUpdaterRunningState();
+	}
+
+	/**
+	 * Removes the action bar message displayed to the given player.
+	 *
+	 * @param player The player.
+	 */
+	public static void removeMessage(Player player)
+	{
+		removeMessage(player, false);
+	}
+
+	/**
+	 * Removes the action bar message displayed to the given player.
+	 *
+	 * @param playerUUID The UUID of the player.
+	 */
+	public static void removeMessage(UUID playerUUID)
+	{
+		removeMessage(playerUUID, false);
+	}
+
+	/**
+	 * Initializes the {@link Runnable} that will re-send the permanent action messages
+	 * to the players.
+	 */
+	private static void initActionMessageUpdater()
+	{
+		actionMessagesUpdater = () -> {
+			for (Map.Entry<UUID, String> entry : actionMessages.entrySet())
+			{
+				Player player = Bukkit.getPlayer(entry.getKey());
+				if (player != null && player.isOnline())
+				{
+					sendMessage(player, entry.getValue());
+				}
+			}
+		};
+	}
+
+	/**
+	 * Checks if the task sending the permanent actions message needs to run and is not
+	 * running, or is useless and running. Stops or launches the task if needed.
+	 */
+	private static void checkActionMessageUpdaterRunningState()
+	{
+		int messagesCount = actionMessages.size();
+
+		if(messagesCount == 0 && actionMessagesUpdaterRunning)
+		{
+			actionMessagesUpdaterTask.cancel();
+			actionMessagesUpdaterTask = null;
+			actionMessagesUpdaterRunning = false;
+		}
+		else if(messagesCount > 0 && !actionMessagesUpdaterRunning)
+		{
+			actionMessagesUpdaterTask = Bukkit.getScheduler().runTaskTimer(SamaGamesAPI.get().getPlugin(), actionMessagesUpdater, 2l, 30l);
+			actionMessagesUpdaterRunning = true;
+		}
+	}
 }
