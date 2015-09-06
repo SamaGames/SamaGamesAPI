4c9a9daac2196af48aa4266c6f41545a4640ec70
Amaury Carrade <amaury@carrade.eu>
2015-09-06 14:11:05 +0200

    Tiny optimization on ColorUtils.
    * OPT: `else if`s instead of many excluding `if`s.

--- src/main/java/net/samagames/tools/ColorUtils.java
+++ src/main/java/net/samagames/tools/ColorUtils.java
@@ -11,52 +11,52 @@ public class ColorUtils
         if (i == 1) {
             c = Color.AQUA;
         }
-        if (i == 2) {
+        else if (i == 2) {
             c = Color.BLACK;
         }
-        if (i == 3) {
+        else if (i == 3) {
             c = Color.BLUE;
         }
-        if (i == 4) {
+        else if (i == 4) {
             c = Color.FUCHSIA;
         }
-        if (i == 5) {
+        else if (i == 5) {
             c = Color.GRAY;
         }
-        if (i == 6) {
+        else if (i == 6) {
             c = Color.GREEN;
         }
-        if (i == 7) {
+        else if (i == 7) {
             c = Color.LIME;
         }
-        if (i == 8) {
+        else if (i == 8) {
             c = Color.MAROON;
         }
-        if (i == 9) {
+        else if (i == 9) {
             c = Color.NAVY;
         }
-        if (i == 10) {
+        else if (i == 10) {
             c = Color.OLIVE;
         }
-        if (i == 11) {
+        else if (i == 11) {
             c = Color.ORANGE;
         }
-        if (i == 12) {
+        else if (i == 12) {
             c = Color.PURPLE;
         }
-        if (i == 13) {
+        else if (i == 13) {
             c = Color.RED;
         }
-        if (i == 14) {
+        else if (i == 14) {
             c = Color.SILVER;
         }
-        if (i == 15) {
+        else if (i == 15) {
             c = Color.TEAL;
         }
-        if (i == 16) {
+        else if (i == 16) {
             c = Color.WHITE;
         }
-        if (i == 17) {
+        else if (i == 17) {
             c = Color.YELLOW;
         }
 
