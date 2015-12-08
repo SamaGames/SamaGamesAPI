e25e76ebac9dfd8cc07cb10e3477a20daf5fc9bc
Silvanosky <geekpower14@gmail.com>
2015-12-08 11:46:13 +0100

    Remove useless line

--- src/main/java/net/samagames/api/player/IFinancialCallback.java
+++ src/main/java/net/samagames/api/player/IFinancialCallback.java
@@ -3,7 +3,6 @@ package net.samagames.api.player;
 /**
  * This file is a part of the SamaGames project
  * This code is absolutely confidential.
- * Created by zyuiop
  * (C) Copyright Elydra Network 2015
  * All rights reserved.
  */
