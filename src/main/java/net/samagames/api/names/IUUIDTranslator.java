6145864e0d4e6bddbacfbca04e6c13ef0aa632bd
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 01:24:04 +0100

    Added javadoc to package names

--- src/main/java/net/samagames/api/names/IUUIDTranslator.java
+++ src/main/java/net/samagames/api/names/IUUIDTranslator.java
@@ -3,36 +3,54 @@ package net.samagames.api.names;
 import java.util.UUID;
 
 /**
- * This file is a part of the SamaGames project
- * This code is absolutely confidential.
- * Created by zyuiop
- * (C) Copyright Elydra Network 2015
- * All rights reserved.
+ * UUID translator class
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
  */
-public interface IUUIDTranslator {
-
+public interface IUUIDTranslator
+{
 	/**
-	 * Renvoie l'UUID d'un joueur en fonction du pseudo
-	 * @param name 				Pseudo du joueur recherché
-	 * @param allowMojangCheck  Autoriser les requêtes vers l'API Mojang
-	 * @return l'uuid du joueur demandé
+	 * Get the UUID of a given username
+     *
+	 * @param name Username
+	 * @param allowMojangCheck Allow Mojang asking
+     *
+	 * @return UUID
 	 */
-	public UUID getUUID(String name, boolean allowMojangCheck);
+    UUID getUUID(String name, boolean allowMojangCheck);
 
-	public default UUID getUUID(String name) {
-		return getUUID(name, false);
+    /**
+     * Get the UUID of a given username
+     *
+     * @param name Username
+     *
+     * @return UUID
+     */
+    default UUID getUUID(String name)
+    {
+		return this.getUUID(name, false);
 	}
 
-	/**
-	 * Renvoie le nom d'un joueur en fonction de son UUID
-	 * @param uuid 				UUID du joueur recherché
-	 * @param allowMojangCheck  Autoriser les requêtes vers l'API Mojang
-	 * @return le pseudo du joueur demandé
-	 */
-	public String getName(UUID uuid, boolean allowMojangCheck);
+    /**
+     * Get the username of a given UUID
+     *
+     * @param uuid UUID
+     * @param allowMojangCheck Allow Mojang asking
+     *
+     * @return Username
+     */
+    String getName(UUID uuid, boolean allowMojangCheck);
 
-	public default String getName(UUID uuid) {
-		return getName(uuid, false);
+    /**
+     * Get the username of a given UUID
+     *
+     * @param uuid UUID
+     *
+     * @return Username
+     */
+    default String getName(UUID uuid)
+    {
+		return this.getName(uuid, false);
 	}
-
 }
