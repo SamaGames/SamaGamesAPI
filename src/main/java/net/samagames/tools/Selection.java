0151164bbb0b81d87a29c88bd1fec2639a464e4b
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 20:01:06 +0100

    Added javadoc for Selection

--- src/main/java/net/samagames/tools/Selection.java
+++ src/main/java/net/samagames/tools/Selection.java
@@ -2,32 +2,54 @@ package net.samagames.tools;
 
 import org.bukkit.Location;
 
+/**
+ * Selection utils
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
+ */
 public class Selection
 {
     private Location firstPoint;
     private Location lastPoint;
 
+    /**
+     * Constructor
+     *
+     * @param firstPoint First point of the selection
+     * @param lastPoint Last point of the selection
+     */
     public Selection(Location firstPoint, Location lastPoint)
     {
         this.firstPoint = firstPoint;
         this.lastPoint = lastPoint;
     }
 
-    public Selection()
-    {
-        this(null, null);
-    }
-
+    /**
+     * Set the first point of the selection
+     *
+     * @param firstPoint First point
+     */
     public void setFirstPoint(Location firstPoint)
     {
         this.firstPoint = firstPoint;
     }
 
+    /**
+     * Set the last point of the selection
+     *
+     * @param lastPoint Last point
+     */
     public void setLastPoint(Location lastPoint)
     {
         this.lastPoint = lastPoint;
     }
 
+    /**
+     * Get the lower point
+     *
+     * @return A point
+     */
     public Location getMinimumPoint()
     {
         double x = Math.min(this.firstPoint.getX(), this.lastPoint.getBlockX());
@@ -37,6 +59,11 @@ public class Selection
         return new Location(this.firstPoint.getWorld(), x, y, z);
     }
 
+    /**
+     * Get the higher point
+     *
+     * @return A point
+     */
     public Location getMaximumPoint()
     {
         double x = Math.max(this.firstPoint.getX(), this.lastPoint.getBlockX());
