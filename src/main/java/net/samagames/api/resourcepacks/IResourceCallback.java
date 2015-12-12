3b364cba5a6126aa61b448bd274cb7513128089c
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 18:24:22 +0100

    Added javadoc to the package resourcepacks

--- src/main/java/net/samagames/api/resourcepacks/IResourceCallback.java
+++ src/main/java/net/samagames/api/resourcepacks/IResourceCallback.java
@@ -4,25 +4,27 @@ import net.minecraft.server.v1_8_R3.PacketPlayInResourcePackStatus;
 import org.bukkit.entity.Player;
 
 /**
- * This file is a part of the SamaGames project
- * This code is absolutely confidential.
- * Created by zyuiop
- * (C) Copyright Elydra Network 2015
- * All rights reserved.
+ * Resource packs callback class
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
  */
-public interface IResourceCallback {
-
+public interface IResourceCallback
+{
 	/***
 	 * Called when the download status is changed
+     *
 	 * @param player THe player downloading the pack
 	 * @param status The download status
 	 */
-	public void callback(Player player, PacketPlayInResourcePackStatus.EnumResourcePackStatus status);
+    void callback(Player player, PacketPlayInResourcePackStatus.EnumResourcePackStatus status);
 
 	/**
 	 * Called when a player is about to be automatically kicked because he doesn't have the pack
+     *
 	 * @param player The player to be kicked
-	 * @return true to kick the player, false to cancel
+     *
+	 * @return {@code true} to kick the player, false to cancel
 	 */
-	public boolean automaticKick(Player player);
+    boolean automaticKick(Player player);
 }
--- src/main/java/net/samagames/api/resourcepacks/IResourcePacksManager.java
+++ src/main/java/net/samagames/api/resourcepacks/IResourcePacksManager.java
@@ -1,37 +1,39 @@
 package net.samagames.api.resourcepacks;
 
 /**
- * This file is a part of the SamaGames project
- * This code is absolutely confidential.
- * Created by zyuiop
- * (C) Copyright Elydra Network 2015
- * All rights reserved.
+ * Resource packs manager
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
  */
-public interface IResourcePacksManager {
-
+public interface IResourcePacksManager
+{
 	/**
 	 * Forces the use of the pack given in argument
+     *
 	 * @param name The name of the pack you want to force the users to use
 	 */
-	public void forcePack(String name);
+    void forcePack(String name);
 
 	/**
 	 * Forces the use of the pack given in argument
+     *
 	 * @param name The name of the pack you want to force the users to use
 	 * @param callback a callback called when the download status changes
 	 */
-	public void forcePack(String name, IResourceCallback callback);
+    void forcePack(String name, IResourceCallback callback);
 
 	/**
 	 * Forces the use of the pack given in argument
+	 *
 	 * @param url The url of the pack you want to force the users to use
 	 * @param hash The hash of the pack you want to force the users to use
 	 * @param callback a callback called when the download status changes
 	 */
-	public void forcePack(String url, String hash, IResourceCallback callback);
+    void forcePack(String url, String hash, IResourceCallback callback);
 
 	/**
 	 * Kicks all the player who didn't totally downloaded the resource pack
 	 */
-	public void kickAllUndownloaded();
+    void kickAllUndownloaded();
 }
