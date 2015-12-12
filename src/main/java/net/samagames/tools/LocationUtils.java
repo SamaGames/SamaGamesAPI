a2bb505cbb949768d21a1137ea43c9bb7c4b0804
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 20:49:15 +0100

    Added javadoc for LocationUtils

--- src/main/java/net/samagames/tools/LocationUtils.java
+++ src/main/java/net/samagames/tools/LocationUtils.java
@@ -3,8 +3,22 @@ package net.samagames.tools;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
+/**
+ * Location utils
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
+ */
 public class LocationUtils
 {
+    /**
+     * Parse a structured string into a location
+     * {@link Location}
+     *
+     * @param loc Structured string
+     *
+     * @return Location instance
+     */
     public static Location str2loc(String loc)
     {
         if (loc == null)
@@ -18,6 +32,13 @@ public class LocationUtils
             return new Location(Bukkit.getServer().getWorld(location[0]), Double.parseDouble(location[1]), Double.parseDouble(location[2]), Double.parseDouble(location[3]));
     }
 
+    /**
+     * Format a location into a structured string
+     *
+     * @param loc Location
+     *
+     * @return Structured string
+     */
     public static String loc2str(Location loc)
     {
         return loc.getWorld().getName() + ", " + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + ", " + loc.getYaw() + ", " + loc.getPitch();
