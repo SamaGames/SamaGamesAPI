8e88f6c7c5d57359f6ca9bb75e5e4394adfc65b9
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 19:11:37 +0100

    Added javadoc to SamaGamesAPI.java

--- src/main/java/net/samagames/api/SamaGamesAPI.java
+++ src/main/java/net/samagames/api/SamaGamesAPI.java
@@ -16,6 +16,7 @@ import net.samagames.api.resourcepacks.IResourcePacksManager;
 import net.samagames.api.settings.ISettingsManager;
 import net.samagames.api.shops.AbstractShopsManager;
 import net.samagames.api.stats.AbstractStatsManager;
+import net.samagames.api.stats.IStatsManager;
 import net.samagames.tools.BarAPI.BarAPI;
 import net.samagames.tools.npc.NPCManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,60 +25,204 @@ import redis.clients.jedis.Jedis;
 import java.util.logging.Level;
 
 /**
- * This file is a part of the SamaGames project
- * This code is absolutely confidential.
- * (C) Copyright Elydra Network 2015
- * All rights reserved.
+ * SamaGames API class
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
  */
-public abstract class SamaGamesAPI {
-
+public abstract class SamaGamesAPI
+{
 	private static SamaGamesAPI instance;
     private JavaPlugin plugin;
 
-	protected SamaGamesAPI(JavaPlugin pluginn) {
-		instance = this;
-        plugin = pluginn;
-    }
-
-	public static SamaGamesAPI get() {
-		return instance;
-	}
-
-    public JavaPlugin getPlugin()
+    /**
+     * Constructor
+     *
+     * @param plugin Root plugin
+     */
+	public SamaGamesAPI(JavaPlugin plugin)
     {
-        return this.plugin;
+		instance = this;
+        this.plugin = plugin;
     }
 
+    /**
+     * Log a given message at the current level into
+     * the console
+     *
+     * @param level Log level
+     * @param text Log test
+     */
     public void log(Level level, String text)
     {
         this.plugin.getLogger().log(level, text);
     }
 
+    /**
+     * Get the server name (for exemple UHCRun_435fvb345)
+     *
+     * @return Server's name
+     */
 	public abstract String getServerName();
 
+    /**
+     * Get the server options
+     *
+     * @return Server's options
+     */
+    public abstract ServerOptions getServerOptions();
+
+    /**
+     * Get an instance of the Redis database
+     *
+     * @return Jedis {@link Jedis} instance
+     */
     public abstract Jedis getBungeeResource();
 
-	public abstract AbstractStatsManager getStatsManager(String game);
+    /**
+     * Get a new instance of the shop manager of
+     * a given game code name
+     *
+     * @param game Game code name
+     *
+     * @return New instance
+     */
 	public abstract AbstractShopsManager getShopsManager(String game);
+
+    /**
+     * Get a new instance of the stats manager of
+     * a given game code name
+     *
+     * @param game Game code name
+     *
+     * @return New instance
+     */
+    public abstract IStatsManager getStatsManager(String game);
+
+    /**
+     * Get the instance of the GUI manager
+     *
+     * @return Instance
+     */
 	public abstract IGuiManager getGuiManager();
+
+    /**
+     * Get the instance of the settings manager
+     *
+     * @return Instance
+     */
 	public abstract ISettingsManager getSettingsManager();
+
+    /**
+     * Get the instance of the player data manager
+     *
+     * @return Instance
+     */
 	public abstract IPlayerDataManager getPlayerManager();
+
+    /**
+     * Get the instance of the achievement manager
+     *
+     * @return Instance
+     */
     public abstract IAchievementManager getAchievementManager();
-	public abstract IUUIDTranslator getUUIDTranslator();
+
+    /**
+     * Get the instance of the game manager
+     *
+     * @return Instance
+     */
 	public abstract IGameManager getGameManager();
+
+    /**
+     * Get the instance of the Redis PubSub API
+     *
+     * @return Instance
+     */
     public abstract IPubSubAPI getPubSub();
+
+    /**
+     * Get the instance of the resource packs manager
+     *
+     * @return Instance
+     */
 	public abstract IResourcePacksManager getResourcePacksManager();
+
+    /**
+     * Get the instance of the friends manager
+     *
+     * @return Instance
+     */
+    @Deprecated
     public abstract IFriendsManager getFriendsManager();
 
-	public abstract IProxyDataManager getProxyDataManager();
-	public abstract IPartiesManager getPartiesManager();
-
-	public abstract BarAPI getBarAPI();
-	public abstract IJoinManager getJoinManager();
-
+    /**
+     * Get the instance of the permissions manager manager
+     *
+     * @return Instance
+     */
 	public abstract IPermissionsManager getPermissionsManager();
 
-	public abstract NPCManager getNPCManager();
+    /**
+     * Get the instance of the join manager
+     *
+     * @return Instance
+     */
+    public abstract IJoinManager getJoinManager();
 
-	public abstract ServerOptions getServerOptions();
+    /**
+     * Get the instance of the proxy data manager
+     *
+     * @return Instance
+     */
+	public abstract IProxyDataManager getProxyDataManager();
+
+    /**
+     * Get the instance of the parties manager
+     *
+     * @return Instance
+     */
+	public abstract IPartiesManager getPartiesManager();
+
+    /**
+     * Get the instance of the NPC manager
+     *
+     * @return Instance
+     */
+    public abstract NPCManager getNPCManager();
+
+    /**
+     * Get the instance of the UUID translator
+     *
+     * @return Instance
+     */
+    public abstract IUUIDTranslator getUUIDTranslator();
+
+    /**
+     * Get the instance of the BarAPI who permits
+     * to add dragon bar based messages
+     *
+     * @return Instance
+     */
+	public abstract BarAPI getBarAPI();
+
+    /**
+     * Get the root plugin of the API
+     *
+     * @return Root plugin instance
+     */
+    public JavaPlugin getPlugin()
+    {
+        return this.plugin;
+    }
+
+    /**
+     * Get the instance of the API
+     *
+     * @return Instance
+     */
+    public static SamaGamesAPI get()
+    {
+        return instance;
+    }
 }
