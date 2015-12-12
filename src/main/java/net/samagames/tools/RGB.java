b42c544af2e7e92d21ee971c9573103d19939b43
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 20:02:50 +0100

    Added javadoc for RGB

--- src/main/java/net/samagames/tools/RGB.java
+++ src/main/java/net/samagames/tools/RGB.java
@@ -1,26 +1,54 @@
 package net.samagames.tools;
 
+/**
+ * RGB utils
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
+ */
 public class RGB
 {
     private final int r, g, b;
-    
+
+    /**
+     * Constructor
+     *
+     * @param r Red color (max 255)
+     * @param g Green color (max 255)
+     * @param b Blue color (max 255)
+     */
     public RGB(int r, int g, int b)
     {
         this.r = r;
         this.g = g;
         this.b = b;
     }
-    
+
+    /**
+     * Get red value
+     *
+     * @return Red color (between 0 and 255)
+     */
     public int getRed()
     {
         return this.r;
     }
-    
+
+    /**
+     * Get green value
+     *
+     * @return Green color (between 0 and 255)
+     */
     public int getGreen()
     {
         return this.g;
     }
-    
+
+    /**
+     * Get blue value
+     *
+     * @return Blue color (between 0 and 255)
+     */
     public int getBlue()
     {
         return this.b;
