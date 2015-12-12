64b5a5241639656f1274de2b1c52b9a58d8cc97d
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 20:41:34 +0100

    Added javadoc for PlayerUtils

--- src/main/java/net/samagames/tools/PlayerUtils.java
+++ src/main/java/net/samagames/tools/PlayerUtils.java
@@ -6,23 +6,59 @@ import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
+/**
+ * Player utils
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
+ */
 public class PlayerUtils
 {
+    /**
+     * Get a fully formatted player name with
+     * rank and color
+     *
+     * @param uuid Player's UUID
+     *
+     * @return Formatted name
+     */
     public static String getFullyFormattedPlayerName(UUID uuid)
     {
         return SamaGamesAPI.get().getPermissionsManager().getPrefix(SamaGamesAPI.get().getPermissionsManager().getApi().getUser(uuid)) + SamaGamesAPI.get().getPermissionsManager().getDisplay(SamaGamesAPI.get().getPermissionsManager().getApi().getUser(uuid)) + SamaGamesAPI.get().getUUIDTranslator().getName(uuid, true) + ChatColor.RESET;
     }
 
+    /**
+     * Get a fully formatted player name with
+     * rank and color
+     *
+     * @param player Player
+     *
+     * @return Formatted name
+     */
     public static String getFullyFormattedPlayerName(Player player)
     {
         return getFullyFormattedPlayerName(player.getUniqueId());
     }
-    
+
+    /**
+     * Get a colored formatted player name
+     *
+     * @param uuid Player's UUID
+     *
+     * @return Formatted name
+     */
     public static String getColoredFormattedPlayerName(UUID uuid)
     {
         return SamaGamesAPI.get().getPermissionsManager().getPrefix(SamaGamesAPI.get().getPermissionsManager().getApi().getUser(uuid)) + SamaGamesAPI.get().getUUIDTranslator().getName(uuid, true) + ChatColor.RESET;
     }
 
+    /**
+     * Get a colored formatted player name
+     *
+     * @param player Player
+     *
+     * @return Formatted name
+     */
     public static String getColoredFormattedPlayerName(Player player)
     {
         return getColoredFormattedPlayerName(player.getUniqueId());
