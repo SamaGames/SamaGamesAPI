2c42fbe0b4794dbec4e38ede1f380765b02bd751
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 17:17:57 +0100

    Added javadoc to the package parties

--- src/main/java/net/samagames/api/parties/IPartiesManager.java
+++ src/main/java/net/samagames/api/parties/IPartiesManager.java
@@ -4,18 +4,46 @@ import java.util.HashMap;
 import java.util.UUID;
 
 /**
- * This file is a part of the SamaGames project
- * This code is absolutely confidential.
- * Created by zyuiop
- * (C) Copyright Elydra Network 2015
- * All rights reserved.
+ * Parties manager class
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
  */
-public interface IPartiesManager {
+public interface IPartiesManager
+{
+    /**
+     * Get the UUID of the given player's party
+     *
+     * @param player Player
+     *
+     * @return
+     */
 	UUID getPlayerParty(UUID player);
 
+    /**
+     * Get members of the given player's party
+     *
+     * @param party Party UUID {@link IPartiesManager##getPlayerParty(UUID)}
+     *
+     * @return A map with UUID and username
+     */
 	HashMap<UUID, String> getPlayersInParty(UUID party);
 
+    /**
+     * Get the server where the party is
+     *
+     * @param party Party UUID {@link IPartiesManager##getPlayerParty(UUID)}
+     *
+     * @return Server name
+     */
 	String getCurrentServer(UUID party);
 
+    /**
+     * Get the UUID of the leader of the party
+     *
+     * @param party Party UUID {@link IPartiesManager##getPlayerParty(UUID)}
+     *
+     * @return Leader UUID
+     */
 	UUID getLeader(UUID party);
 }
