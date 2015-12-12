0111a62e9097fc9a456f9696480d8e1cf13c1ae8
BlueSlime <jeremylevilain78@orange.fr>
2015-12-12 20:59:09 +0100

    Added javadoc for ItemUtils

--- src/main/java/net/samagames/tools/ItemUtils.java
+++ src/main/java/net/samagames/tools/ItemUtils.java
@@ -5,19 +5,48 @@ import org.bukkit.SkullType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
+/**
+ * ItemUtils
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
+ */
 public class ItemUtils
 {
+    /**
+     * Format a given object into a formatted
+     * string
+     *
+     * @param stack Stack
+     *
+     * @return Formatted string
+     */
     public static String stackToStr(ItemStack stack)
     {
         return stack.getType().name() + ", " + stack.getDurability();
     }
 
+    /**
+     * Format a given formatted string into
+     * a object {@link ItemStack}
+     *
+     * @param string
+     *
+     * @return
+     */
     public static ItemStack strToStack(String string)
     {
         String[] data = string.split(", ");
         return new ItemStack(Material.matchMaterial(data[0]), 1, Short.valueOf(data[1]));
     }
 
+    /**
+     * Get the given player's username head
+     *
+     * @param player Player's username
+     *
+     * @return Player's head
+     */
     public static ItemStack getPlayerHead(String player)
     {
         ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
