46ca15a05fb1be3518ffdd638e0508cb5472a6ba
Silvanosky <geekpower14@gmail.com>
2015-12-18 23:40:53 +0100

    NPC update

--- src/main/java/net/samagames/tools/npc/NPCManager.java
+++ src/main/java/net/samagames/tools/npc/NPCManager.java
@@ -4,10 +4,16 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.server.v1_8_R3.World;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.tools.CallBack;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +24,7 @@ import java.util.concurrent.ScheduledExecutorService;
 /**
  * Created by Silva on 20/10/2015.
  */
-public class NPCManager {
+public class NPCManager  implements Listener{
 
     public SamaGamesAPI api;
 
@@ -33,12 +39,9 @@ public class NPCManager {
     public NPCManager(SamaGamesAPI api)
     {
         this.api = api;
-        init();
-    }
-
-    public void init()
-    {
         scheduler = Executors.newScheduledThreadPool(2);
+
+        Bukkit.getPluginManager().registerEvents(this, api.getPlugin());
     }
 
     public CustomNPC createNPC(Location location, UUID uuid)
@@ -52,6 +55,14 @@ public class NPCManager {
             scoreBoardRegister.done(npc, null);
 
         return npc;
+    }
+
+    public void removeNPC(String name)
+    {
+        CustomNPC npc = getNPCEntity(name);
+        if(npc != null)
+            npc.getWorld().removeEntity(npc);
+
     }
 
     public void disable()
@@ -73,5 +84,25 @@ public class NPCManager {
 
     public void setScoreBoardRegister(CallBack<CustomNPC> scoreBoardRegister) {
         this.scoreBoardRegister = scoreBoardRegister;
+    }
+
+    @EventHandler
+    public void onPlayerHitNPC(EntityDamageByEntityEvent event)
+    {
+        if(event.getEntity() instanceof CustomNPC && event.getDamager() instanceof Player)
+        {
+            CustomNPC npc = (CustomNPC) event.getEntity();
+            npc.onInteract(false, (Player) event.getDamager());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteractNPC(PlayerInteractEntityEvent event)
+    {
+        if(event.getRightClicked() instanceof CustomNPC)
+        {
+            CustomNPC npc = (CustomNPC) event.getRightClicked();
+            npc.onInteract(true, event.getPlayer());
+        }
     }
 }
