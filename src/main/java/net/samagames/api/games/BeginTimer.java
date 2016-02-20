d705c72b3fe43bd0c6e8d6b1857285bf0ade51b5
BlueSlime <jeremylevilain78@orange.fr>
2016-02-20 11:50:27 +0100

    Added a new flag for the games to disable automatic mechanisms like begin countdown

--- src/main/java/net/samagames/api/games/BeginTimer.java
+++ src/main/java/net/samagames/api/games/BeginTimer.java
@@ -30,8 +30,14 @@ public class BeginTimer implements Runnable
     {
         this.game = game;
         this.api = SamaGamesAPI.get();
-        this.time = timeStart;
+        this.time = this.timeStart;
         this.ready = false;
+
+        if (this.api.getGameManager().isFreeMode())
+        {
+            this.game.getBeginTimer().cancel();
+            return;
+        }
     }
 
     /**
@@ -47,6 +53,12 @@ public class BeginTimer implements Runnable
     @Override
     public void run()
     {
+        if (this.api.getGameManager().isFreeMode())
+        {
+            this.game.getBeginTimer().cancel();
+            return;
+        }
+
         int nPlayers = this.game.getConnectedPlayers();
 
         if (nPlayers >= this.api.getGameManager().getGameProperties().getMinSlots() && !this.ready)
