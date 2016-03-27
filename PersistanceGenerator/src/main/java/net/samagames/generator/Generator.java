16a77c9fe5acb762ca43020b2e95145fe1141103
Silvanosky <geekpower14@gmail.com>
2016-03-27 04:59:49 +0200

    remove files and add generator

--- PersistanceGenerator/src/main/java/net/samagames/generator/Generator.java
+++ PersistanceGenerator/src/main/java/net/samagames/generator/Generator.java
@@ -1,40 +1,103 @@
 package net.samagames.generator;
 
-import com.squareup.javapoet.FieldSpec;
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.*;
+import net.samagames.persistanceapi.beans.statistics.PlayerStatisticsBean;
 
 import javax.lang.model.element.Modifier;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Silvanosky on 25/03/2016.
  */
 public class Generator {
 
+    private static List<JavaFile> toBuild = new ArrayList<>();
+
     public static void main(String[] args)
     {
-        TypeSpec helloWorld = TypeSpec.interfaceBuilder("HelloWorld")
-                .addModifiers(Modifier.PUBLIC)
-                .addField(FieldSpec.builder(String.class, "ONLY_THING_THAT_IS_CONSTANT")
-                        .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
-                        .initializer("$S", "change")
-                        .build())
-                .addMethod(MethodSpec.methodBuilder("beep")
-                        .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
-                        .build())
-                .build();
-        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
-                .build();
+        //
+        loadGameStats();
 
+        build();
+    }
+
+    public static void loadGameStats()
+    {
+        Field[] playerStatisticFields = PlayerStatisticsBean.class.getDeclaredFields();
+        TypeSpec.Builder playerStatsBuilder = TypeSpec.interfaceBuilder("IPlayerStats")
+                .addModifiers(Modifier.PUBLIC);
+
+        playerStatsBuilder.addMethod(getMethod("updateStats", void.class));
+        playerStatsBuilder.addMethod(getMethod("refreshStats", boolean.class));
+        playerStatsBuilder.addMethod(getMethod("getPlayerUUID", UUID.class));
+
+        for (Field field : playerStatisticFields)
+        {
+            field.setAccessible(true);
+            Class workingField = field.getType();
+            String name = "I" + workingField.getSimpleName().replaceAll("Bean", "");
+
+            TypeSpec.Builder object = TypeSpec.interfaceBuilder(name)
+                    .addModifiers(Modifier.PUBLIC);
+            Method[] subDeclaredMethods = workingField.getDeclaredMethods();
+            for (Method method : subDeclaredMethods)
+            {
+                MethodSpec.Builder builder = MethodSpec.methodBuilder(method.getName());
+                if (method.getParameterCount() > 0)
+                {
+                    for (Parameter parameter : method.getParameters())
+                    {
+                        builder.addParameter(parameter.getType(), parameter.getName());
+                    }
+                }
+                builder.addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT);
+                builder.returns(method.getReturnType());
+                object.addMethod(builder.build());
+            }
+            TypeSpec build = object.build();
+            String pack = "net.samagames.api.stats.games";
+            ClassName className = ClassName.get(pack, name);
+
+            //Create getter in player stat
+            playerStatsBuilder.addMethod(getMethod("get" + name, className));
+
+            toBuild.add(JavaFile.builder("net.samagames.api.stats.games", build).build());
+        }
+        toBuild.add(JavaFile.builder("net.samagames.api.stats", playerStatsBuilder.build()).build());
+    }
+
+    public static void build()
+    {
         try {
             File file = new File("./Generation");
             file.delete();
-            javaFile.writeTo(file);
+            for (JavaFile javaFile : toBuild)
+            {
+                javaFile.writeTo(file);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static MethodSpec getMethod(String name, TypeName retur)
+    {
+        MethodSpec.Builder getter = MethodSpec.methodBuilder(name);
+        getter.addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT);
+        getter.returns(retur);
+        return getter.build();
+    }
+
+    public static MethodSpec getMethod(String name, Type retur)
+    {
+        return getMethod(name, TypeName.get(retur));
     }
 }
