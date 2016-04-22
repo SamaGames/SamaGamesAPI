d6ced5b67e0080bc4ded219cae59ecc47c975be7
IamBlueSlime <iamblueslime@gmail.com>
2016-04-22 05:26:50 +0200

    Testing colors for the BossBar

--- src/main/java/net/samagames/api/games/GamePlayer.java
+++ src/main/java/net/samagames/api/games/GamePlayer.java
@@ -49,7 +49,7 @@ public class GamePlayer
     public void handleLogin(boolean reconnect)
     {
         SamaGamesAPI.get().getGameManager().getCoherenceMachine().getMessageManager().writeWelcomeInGameToPlayer(this.getPlayerIfOnline());
-        BossBarAPI.setBar(this.getPlayerIfOnline(), ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + "!", BarColor.YELLOW, BarStyle.SOLID, 100.0D);
+        BossBarAPI.setBar(this.getPlayerIfOnline(), ChatColor.RED + "Vous jouez sur " + ChatColor.DARK_RED + "mc.samagames.net" + ChatColor.RED + " !", BarColor.RED, BarStyle.SOLID, 100.0D);
     }
 
     /**
