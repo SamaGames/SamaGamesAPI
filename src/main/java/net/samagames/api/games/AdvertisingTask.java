c54d5f1a87d315226b226f8fc3caab169abc5655
BlueSlime <jeremylevilain78@orange.fr>
2016-05-30 18:28:28 +0200

    Added 2 more styles to the advertizing task

--- src/main/java/net/samagames/api/games/AdvertisingTask.java
+++ src/main/java/net/samagames/api/games/AdvertisingTask.java
@@ -12,36 +12,63 @@ import org.bukkit.scheduler.BukkitRunnable;
 class AdvertisingTask extends BukkitRunnable
 {
     private final BossBar advertisingBossBar;
+    private int style;
     private int loop;
 
     AdvertisingTask()
     {
         this.advertisingBossBar = BossBarAPI.getBar(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !", BarColor.RED, BarStyle.SOLID, 100.0D).getValue();
+        this.style = 0;
         this.loop = 0;
 
-        this.runTaskTimer(SamaGamesAPI.get().getPlugin(), 10L, 10L);
+        this.runTaskTimer(SamaGamesAPI.get().getPlugin(), 0L, 5L);
     }
 
     @Override
     public void run()
     {
-        if (this.loop < 10)
-            this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !");
-        else if (this.loop < 11)
-            this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.RED + "mc.samagames.net" + ChatColor.YELLOW + " !");
-        else if (this.loop < 12)
-            this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !");
-        else if (this.loop < 13)
-            this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.RED + "mc.samagames.net" + ChatColor.YELLOW + " !");
-        else if (this.loop < 14)
-            this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !");
-        else if (this.loop < 15)
-            this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.RED + "mc.samagames.net" + ChatColor.YELLOW + " !");
+        if (this.style == 0)
+        {
+            if (this.loop < 20)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !");
+            else if (this.loop < 22)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.RED + "mc.samagames.net" + ChatColor.YELLOW + " !");
+            else if (this.loop < 24)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !");
+            else if (this.loop < 26)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.RED + "mc.samagames.net" + ChatColor.YELLOW + " !");
+            else if (this.loop < 28)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !");
+            else if (this.loop < 30)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.RED + "mc.samagames.net" + ChatColor.YELLOW + " !");
+        }
+        else if (this.style == 1)
+        {
+            if (this.loop < 20)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !");
+            else if (this.loop < 34)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + this.upIpAt(this.loop) + ChatColor.YELLOW + " !");
+        }
+        else if (this.style == 2)
+        {
+            if (this.loop < 20)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + "mc.samagames.net" + ChatColor.YELLOW + " !");
+            else if (this.loop < 34)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + this.colorIpAt(ChatColor.RED, this.loop) + ChatColor.YELLOW + " !");
+            else if (this.loop < 48)
+                this.advertisingBossBar.setTitle(ChatColor.YELLOW + "Vous jouez sur " + ChatColor.GOLD + this.colorIpAt(ChatColor.GOLD, this.loop) + ChatColor.YELLOW + " !");
+        }
 
         this.loop++;
 
         if (this.loop >= 15)
+        {
             this.loop = 0;
+            this.style++;
+
+            if (this.style >= 3)
+                this.style = 0;
+        }
     }
 
     public void addPlayer(Player player)
@@ -52,5 +79,21 @@ class AdvertisingTask extends BukkitRunnable
     public void removePlayer(Player player)
     {
         this.advertisingBossBar.removePlayer(player);
+    }
+
+    private String upIpAt(int loop)
+    {
+        int charIndex = loop - 20;
+        String ip = "mc.samagames.net";
+
+        return ip.substring(0, charIndex) + Character.toUpperCase(ip.charAt(charIndex)) + ip.substring(charIndex + 1);
+    }
+
+    private String colorIpAt(ChatColor color, int loop)
+    {
+        int charIndex = loop - 20;
+        String ip = "mc.samagames.net";
+
+        return ip.substring(0, charIndex) + color + ip.charAt(charIndex) + ChatColor.GOLD + ip.substring(charIndex + 1);
     }
 }
