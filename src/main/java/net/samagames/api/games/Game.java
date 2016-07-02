73b8276ca56db3dbb3ac2ee0e034727e7ac84919
BlueSlime <jeremylevilain78@orange.fr>
2016-07-03 01:49:21 +0200

    Added automatic i18n loading

--- src/main/java/net/samagames/api/games/Game.java
+++ src/main/java/net/samagames/api/games/Game.java
@@ -3,6 +3,8 @@ package net.samagames.api.games;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.api.games.themachine.ICoherenceMachine;
 import net.samagames.api.games.themachine.messages.templates.EarningMessageTemplate;
+import net.samagames.api.i18n.I18n;
+import net.samagames.api.i18n.ProjectNames;
 import net.samagames.tools.Titles;
 import org.apache.commons.lang3.tuple.Pair;
 import org.bukkit.Bukkit;
@@ -36,6 +38,7 @@ public class Game<GAMEPLAYER extends GamePlayer>
     protected final String gameName;
     protected final String gameDescription;
     protected final Class<GAMEPLAYER> gamePlayerClass;
+    protected final I18n i18n;
     protected final HashMap<UUID, GAMEPLAYER> gamePlayers;
     protected final HashMap<UUID, GAMEPLAYER> gameSpectators;
     protected final AdvertisingTask advertisingTask;
@@ -54,19 +57,25 @@ public class Game<GAMEPLAYER extends GamePlayer>
      * @param gamePlayerClass The class of your custom {@link GamePlayer} object, the same
      *                        as the {@link GAMEPLAYER} class. Use {@code GamePlayer.class}
      *                        if you are not using a custom class.
+     * @param internationalizationName The enumeration entry of your project to load the sentences
+     *                                 related to your project for the internationalization.
      */
-    public Game(String gameCodeName, String gameName, String gameDescription, Class<GAMEPLAYER> gamePlayerClass)
+    public Game(String gameCodeName, String gameName, String gameDescription, Class<GAMEPLAYER> gamePlayerClass, ProjectNames internationalizationName)
     {
         this.gameManager = SamaGamesAPI.get().getGameManager();
         this.gameCodeName = gameCodeName.toLowerCase();
         this.gameName = gameName;
         this.gameDescription = gameDescription;
         this.gamePlayerClass = gamePlayerClass;
+        this.i18n = SamaGamesAPI.get().getI18n();
         this.gamePlayers = new HashMap<>();
         this.gameSpectators = new HashMap<>();
         this.advertisingTask = new AdvertisingTask();
 
         this.status = Status.WAITING_FOR_PLAYERS;
+
+        if (internationalizationName != null)
+            SamaGamesAPI.get().getI18n().setLanguagesToLoad(internationalizationName, true);
     }
 
     /**
