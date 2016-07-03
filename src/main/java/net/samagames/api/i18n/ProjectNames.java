d78addcabc6f51a5683dfbd151e67b7b8e28870b
BlueSlime <jeremylevilain78@orange.fr>
2016-07-03 02:47:37 +0200

    Changed project ids

--- src/main/java/net/samagames/api/i18n/ProjectNames.java
+++ src/main/java/net/samagames/api/i18n/ProjectNames.java
@@ -6,43 +6,43 @@ public enum ProjectNames
      * Global
      */
 
-    PROXY(0),
-    HUB(1),
+    PROXY(1),
+    HUB(2),
 
 
     /**
      * Long games
      */
 
-    QUAKE(2),
-    UPPERVOID(3),
-    DIMENSIONS(4),
-    HEROBATTLE(5),
-    CHUNKWARS(6),
+    QUAKE(3),
+    UPPERVOID(4),
+    DIMENSIONS(5),
+    HEROBATTLE(6),
+    CHUNKWARS(7),
 
 
     /**
      * Survival games
      */
 
-    UHC(7),
-    UHCRUN(8),
-    DOUBLERUNNER(9),
-    UHCRANDOM(10),
-    RANDOMRUN(11),
+    UHC(8),
+    UHCRUN(9),
+    DOUBLERUNNER(10),
+    UHCRANDOM(11),
+    RANDOMRUN(12),
 
 
     /**
      * Arcade games
      */
 
-    AGARMC(12),
-    HANGOVERGAMES(13),
-    WITHERPARTY(14),
-    BURNTHATCHICKEN(15),
-    PACMAN(16),
-    BOMBERMAN(17),
-    FLYRING(18),
+    AGARMC(13),
+    HANGOVERGAMES(14),
+    WITHERPARTY(15),
+    BURNTHATCHICKEN(16),
+    PACMAN(17),
+    BOMBERMAN(18),
+    FLYRING(19),
     ;
 
     private int value;
