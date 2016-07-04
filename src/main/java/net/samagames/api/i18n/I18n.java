1b03a233645be47109075c21c3ec95297e15dd53
Silvanosky <geekpower14@gmail.com>
2016-07-04 13:39:16 +0200

    Revert "Returning default string instead of an exception if the sentence is not found"
    This reverts commit d078fe4c549af956e91b2e39996d8ed1b5d9ceb2

--- src/main/java/net/samagames/api/i18n/I18n.java
+++ src/main/java/net/samagames/api/i18n/I18n.java
@@ -15,5 +15,5 @@ public interface I18n
      * Get a string localized with the given player language
      * @return Localized string
      */
-    String localize(int sentenceId, UUID player, Object... params);
+    String localize(int sentenceId, UUID player, Object... params) throws Exception;
 }
