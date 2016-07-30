70ae61d55edfe5e79ce69d36edf94e23fc03df80
IamBlueSlime <iamblueslime@gmail.com>
2016-07-30 15:18:13 +0200

    Added Survival games to GamesNames enum

--- src/main/java/net/samagames/api/games/GamesNames.java
+++ src/main/java/net/samagames/api/games/GamesNames.java
@@ -19,12 +19,18 @@ public enum GamesNames {
     HEROBATTLE(1),
     JUKEBOX(2),
     QUAKE(3),
-    UHCRUN(4),
-    UPPERVOID(5),
-    DIMENSION(6),
-    BOWLING(7);
+    UHC(4),
+    UHCRUN(5),
+    DOUBLERUNNER(6),
+    UHCRANDOM(7),
+    RANDOMRUN(8),
+    ULTRAFLAGKEEPER(9),
+    UPPERVOID(10),
+    DIMENSIONS(11),
+    BOWLING(12);
 
     private int value;
+
     GamesNames(int value)
     {
         this.value = value;
