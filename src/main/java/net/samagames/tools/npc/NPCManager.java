98965d6b71fa7a7c3735bc851a352b4767a1bfc3
Rigner <frasse_l@epitech.eu>
2016-08-25 18:13:12 +0200

    Fix

--- src/main/java/net/samagames/tools/npc/NPCManager.java
+++ src/main/java/net/samagames/tools/npc/NPCManager.java
@@ -117,7 +117,7 @@ public class NPCManager implements Listener {
         GameProfile gameProfile = new ProfileLoader(skinUUID.toString(), "[NPC] " + entities.size(), skinUUID).loadProfile();
 
         final CustomNPC npc = new CustomNPC(w, gameProfile, new PlayerInteractManager(w));
-        npc.getBukkitEntity().setDisplayName("");
+        npc.listName = IChatBaseComponent.ChatSerializer.a("");
         npc.setLocation(location);
 
         Hologram hologram = new Hologram(hologramLines);
