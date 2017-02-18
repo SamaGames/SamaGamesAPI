30427e9935c88b13f43584ffcc50ae1ac451dbaa
BlueSlime <jeremylevilain78@orange.fr>
2017-02-18 22:09:03 +0100

    Fix

--- src/main/java/net/samagames/tools/holograms/Hologram.java
+++ src/main/java/net/samagames/tools/holograms/Hologram.java
@@ -307,7 +307,6 @@ public class Hologram
         armorStand.setNoGravity(true);
         armorStand.setCustomName(text);
         armorStand.setCustomNameVisible(true);
-        armorStand.setMarker(true);
         armorStand.setLocation(loc.getX(), loc.getY() - 2, loc.getZ(), 0, 0);
 
         return armorStand;
